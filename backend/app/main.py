ba291c161976da6cad27fedf0ebb1ef5e952c530
Sreedhanya S <131782861+Sree-dhanya@users.noreply.github.com>
2026-05-14 14:28:15 +0530

    aftersubdomain2

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,8 +22,6 @@ else:
     origins = [
         "http://localhost:3000",  # React frontend
         "http://localhost:5173",
-        "https://therapinc.in",
-        "https://www.therapinc.in",
         "https://therapinc.stmarthasspecialschool.com"  # Vite frontend (if using Vite)
     ]
 
